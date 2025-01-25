b3824e7976253a712297bc8692525235ca49e4c1
Auto <chonlatee@dosetech.co>
2025-01-25 17:39:32 +0700

    update: font family

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,20 @@ import { I18nProvider } from "./locales";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { UserProfileImgProvider } from "./provider/user-profile-img";
+import { ThemeProvider } from "@mui/material/styles";
+
+import "./index.css";
+import { theme } from "./provider/font-family";
 
 const App: FC = () => {
   return (
-    <I18nProvider>
-      <UserProfileImgProvider>
-        <RouterProvider router={router} />
-      </UserProfileImgProvider>
-    </I18nProvider>
+    <ThemeProvider theme={theme}>
+      <I18nProvider>
+        <UserProfileImgProvider>
+          <RouterProvider router={router} />
+        </UserProfileImgProvider>
+      </I18nProvider>
+    </ThemeProvider>
   );
 };
 
